e0b5ea774ad2115ae7b4ea154c4abe0fd03c3e85
Wesley <serpents123@gmail.com>
2022-12-28 00:35:15 -0800

    add cypress

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images : {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.akamai.steamstatic.com'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
